172a6010746a7770ddb920d22ad141665a919acf
Gregg Van Hove <gvanhove@pivotal.io>
2018-02-13 15:40:34 -0800

    refactor out all the common stuff in the config producer

--- src/io/pivotal/intellij/jasmine/JasmineRunConfigurationProducer.kt
+++ src/io/pivotal/intellij/jasmine/JasmineRunConfigurationProducer.kt
@@ -6,52 +6,37 @@ import com.intellij.javascript.testing.JsTestRunConfigurationProducer
 import com.intellij.lang.javascript.psi.JSFile
 import com.intellij.openapi.util.Ref
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFileSystemItem
 import com.intellij.psi.util.PsiUtilCore
 import com.intellij.util.ObjectUtils.tryCast
 
 class JasmineRunConfigurationProducer : JsTestRunConfigurationProducer<JasmineRunConfiguration>(JasmineConfigurationType.getInstance(), ImmutableList.of("jasmine")) {
     override fun isConfigurationFromCompatibleContext(runConfig: JasmineRunConfiguration, context: ConfigurationContext): Boolean {
-        val element = context.psiLocation ?: return false
-        val currentFile = PsiUtilCore.getVirtualFile(element) ?: return false
-        if (currentFile.isDirectory) {
-            // not sure the right way to run a dir
-            return false
-        }
+        return findTestFile(runConfig, context) != null
+    }
 
-        val psiFile = tryCast(element.containingFile, JSFile::class.java) ?: return false
-        psiFile.testFileType ?: return false
-
-//        if (element is PsiFileSystemItem) {
-            runConfig.jasmineRunSettings = runConfig.jasmineRunSettings.copy(
-                    specFile = currentFile.path
-            )
-//        } else return false
+    override fun setupConfigurationFromCompatibleContext(runConfig: JasmineRunConfiguration, context: ConfigurationContext, sourceElement: Ref<PsiElement>): Boolean {
+        val jsFile = findTestFile(runConfig, context) ?: return false
+        sourceElement.set(jsFile)
+        runConfig.setGeneratedName()
 
         return true
     }
 
-    override fun setupConfigurationFromCompatibleContext(runConfig: JasmineRunConfiguration, context: ConfigurationContext, sourceElement: Ref<PsiElement>): Boolean {
-        val element = context.psiLocation ?: return false
-        val currentFile = PsiUtilCore.getVirtualFile(element) ?: return false
+    private fun findTestFile(runConfig: JasmineRunConfiguration, context: ConfigurationContext): JSFile? {
+        val element = context.psiLocation ?: return null
+        val currentFile = PsiUtilCore.getVirtualFile(element) ?: return null
         if (currentFile.isDirectory) {
             // not sure the right way to run a dir
-            return false
+            return null
         }
 
-        val psiFile = tryCast(element.containingFile, JSFile::class.java) ?: return false
+        val psiFile = tryCast(element.containingFile, JSFile::class.java) ?: return null
+        psiFile.testFileType ?: return null
 
-        if (element is PsiFileSystemItem) {
-            psiFile.testFileType ?: return false
+        runConfig.jasmineRunSettings = runConfig.jasmineRunSettings.copy(
+                specFile = currentFile.path
+        )
 
-            runConfig.jasmineRunSettings = runConfig.jasmineRunSettings.copy(
-                    specFile = currentFile.path
-            )
-            sourceElement.set(psiFile)
-            runConfig.setGeneratedName()
-        } else return false
-
-
-        return true
+        return psiFile
     }
 }
